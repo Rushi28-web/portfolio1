8bcbf7643d0f0b530b4f06a02673e4c5094329d4
N.Rushi <rushigowda890@gmail.com>
2025-04-05 14:02:47 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var typed = new Typed('.typing', {
+    strings: ["  BCA   Student"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop: true
+})
+
+const navItems = document.querySelectorAll('.nav-item');
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navItems.forEach(nav => nav.classList.remove('active'));
+        item.classList.add('active');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+    const navItems = document.querySelectorAll('.nav-item');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                navItems.forEach(navItem => {
+                    navItem.classList.remove('active');
+                    if (navItem.getAttribute('href').substring(1) === entry.target.id) {
+                        navItem.classList.add('active');
+                    }
+                });
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
